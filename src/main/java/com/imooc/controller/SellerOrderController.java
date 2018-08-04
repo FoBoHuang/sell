2907a0a331d67ed71cbf7dca3c9e968fc6c78c5c
marvHuang <hfb161716@163.com>
2018-08-04 17:50:40 +0800

    [add&revise]添加订单详情、订单取消、订单完结的API(卖家端Controller)

--- src/main/java/com/imooc/controller/SellerOrderController.java
+++ src/main/java/com/imooc/controller/SellerOrderController.java
@@ -1,7 +1,10 @@
 package com.imooc.controller;
 
 import com.imooc.dto.OrderDTO;
+import com.imooc.enums.ResultEnum;
+import com.imooc.exception.SellException;
 import com.imooc.service.OrderService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -45,5 +48,80 @@ public class SellerOrderController {
         map.put("currentPage", page);
         map.put("size", size);
         return new ModelAndView("order/list", map);
+    }
+
+    /**
+     * 取消订单
+     * @param orderId
+     * @return
+     */
+    @GetMapping("/cancel")
+    public ModelAndView cancel(@RequestParam("orderId") String orderId,
+                               Map<String,Object> map){
+
+        try{
+            OrderDTO orderDTO = orderService.findOne(orderId);
+            orderService.cancel(orderDTO);
+        }catch (SellException e){
+
+            log.error("【卖家端取消订单】发生异常{}", e);
+            map.put("msg", e.getMessage());
+            map.put("url", "/seller/order/list");
+            return new ModelAndView("common/error", map);
+        }
+
+        map.put("msg",ResultEnum.ORDER_CANCEL_SUCCESS.getMessage());
+        map.put("url","/seller/order/list");
+        return new ModelAndView("common/success",map);
+    }
+
+    /**
+     * 订单详情
+     * @param orderId
+     * @param map
+     * @return
+     */
+    @GetMapping("/detail")
+    public ModelAndView detail(@RequestParam("orderId") String orderId,
+                               Map<String,Object> map){
+
+        OrderDTO orderDTO = new OrderDTO();
+        try{
+            orderDTO = orderService.findOne(orderId);
+        }catch (SellException e){
+
+            log.error("【卖家端查询订单详情】发生异常{}", e);
+            map.put("msg", e.getMessage());
+            map.put("url", "/seller/order/list");
+            return new ModelAndView("common/error", map);
+        }
+
+        map.put("orderDTO",orderDTO);
+        return new ModelAndView("order/detail",map);
+    }
+
+    /**
+     * 完结订单
+     * @param orderId
+     * @param map
+     * @return
+     */
+    @GetMapping("/finish")
+    public ModelAndView finished(@RequestParam("orderId") String orderId,
+                                 Map<String, Object> map) {
+        try {
+            OrderDTO orderDTO = orderService.findOne(orderId);
+            orderService.finish(orderDTO);
+        } catch (SellException e) {
+
+            log.error("【卖家端完结订单】发生异常{}", e);
+            map.put("msg", e.getMessage());
+            map.put("url", "/seller/order/list");
+            return new ModelAndView("common/error", map);
+        }
+
+        map.put("msg", ResultEnum.ORDER_FINISH_SUCCESS.getMessage());
+        map.put("url", "/seller/order/list");
+        return new ModelAndView("common/success");
     }
 }
