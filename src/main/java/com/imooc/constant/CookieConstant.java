2ce9da3620aa887a4ffa0892a37d9374c4728019
marvHuang <hfb161716@163.com>
2018-08-12 14:47:20 +0800

    [add]添加卖家端登入和登出功能

---
+++ src/main/java/com/imooc/constant/CookieConstant.java
@@ -0,0 +1,15 @@
+package com.imooc.constant;
+
+/**
+ * cookie常量
+ *
+ * @author Huangfobo
+ * @create 2018-08-12 13:54
+ **/
+
+public interface CookieConstant {
+
+    String TOKEN = "token";
+
+    Integer EXPIRE = 7200;
+}
---
+++ src/main/java/com/imooc/constant/RedisConstant.java
@@ -0,0 +1,15 @@
+package com.imooc.constant;
+
+/**
+ * redis常量
+ *
+ * @author Huangfobo
+ * @create 2018-08-12 13:53
+ **/
+
+public interface RedisConstant {
+
+    String TOKEN_PREFIX = "token_%s";
+
+    Integer EXPIRE = 7200; //2小时
+}
